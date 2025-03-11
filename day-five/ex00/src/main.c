13abd5d75ec3535542e6f15e5cb2291afaf8f1c3
Andrew Attali <me@mortebrume.eu>
2025-03-11 13:45:20 +0100

    fix: use prescaler for ex00

--- day-five/ex00/src/main.c
+++ day-five/ex00/src/main.c
@@ -3,6 +3,7 @@
 #include "avr/io.h"
 #include <stdint.h>
 
+// Trigger an interrupt every 2ms.
 void timer_setup() {
   TCCR1B |= (1 << WGM12) | (1 << CS11);
   OCR1A = 40000;
@@ -10,9 +11,15 @@ void timer_setup() {
 }
 
 void adc_setup() {
+  // We use AVCC as reference voltage (p. 257).
+  // Using left adjust result to only read 8 bits.
   ADMUX |= (1 << REFS0) | (1 << ADLAR);
+  // Disable input for the pin associated to ADC0 (p. 261).
   DIDR0 |= (1 << ADC0D);
+  // Enable ADC in free running mode (p. 258).
   ADCSRA |= (1 << ADEN) | (1 << ADATE) | (1 << ADSC);
+  // We use a prescaler of 64 to be around the 200kHz required range (p. 259).
+  ADCSRA |= (1 << ADPS1) | (1 << ADPS2);
 }
 
 int main() {
@@ -28,6 +35,7 @@ int main() {
   return (0);
 }
 
+// Read the convert value from ADC and print it via UART.
 ISR(TIMER1_COMPA_vect) {
   uint8_t value = ADCH;
   uart_printhex(value);
